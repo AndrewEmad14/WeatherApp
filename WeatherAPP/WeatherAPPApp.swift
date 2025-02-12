3b9f6d9d912de1f7bc76211dfd215378acc8d6cf
Andrew Emad <andrew14emad@gmail.com>
2025-02-12 13:54:01 -0800

    Initial Commit

---
+++ WeatherAPP/WeatherAPPApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAPPApp.swift
+//  WeatherAPP
+//
+//  Created by Andrew Emad on 12/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAPPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
